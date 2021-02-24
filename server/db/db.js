fe12dfd3e9b52b9756cc1cd8c1242f7a1cabfbac
williamg1750 <william.guan12@gmail.com>
2021-02-23 21:50:28 -0500

    added update so it can be seeded

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,38 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
-const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
-const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
-const client = new Sequelize(dbUrl, { logging: false, operatorsAliases: false })
+const Sequelize = require('sequelize');
+const pkg = require('../../package.json');
+const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name;
+const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`;
+const client = new Sequelize(dbUrl, {
+  logging: false,
+  operatorsAliases: false,
+});
+// let config;
+// if (process.env.DATABASE_URL) {
+//   config = {
+//     logging: false,
+//     operatorsAliases: false,
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     ssl: true,
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   };
+// } else {
+//   config = {
+//     logging: false,
+//     operatorsAliases: false,
+//   };
+// }
+const client = new Sequelize(dbUrl, config);
 
-module.exports = client
+module.exports = client;
 
 // This is a global Mocha hook used for resource cleanup.
 // Otherwise, Mocha v4+ does not exit after tests.
 if (process.env.NODE_ENV === 'test') {
-  after('close database connection', () => client.close())
+  after('close database connection', () => client.close());
 }
